644f322f6b97167483eda3d7e75ac02c5b208d7a
jrouwe <jrouwe@users.noreply.github.com>
2023-01-27 15:09:11 +0000

    deploy: acbb0e7e324abee709fb1a7cf44de096c2dacd68

--- namespaces_dup.js
+++ namespaces_dup.js
@@ -5,8 +5,8 @@ var namespaces_dup =
       [ "GetBaryCentricCoordinates", "namespace_closest_point.html#a292e497d840ac56d4004212fd10b18e4", null ],
       [ "GetBaryCentricCoordinates", "namespace_closest_point.html#ab3cdc6ef3206b1e17607908ae2ec3543", null ],
       [ "GetClosestPointOnLine", "namespace_closest_point.html#ad687ef98922a03b7128a5d3a5a64ab39", null ],
-      [ "GetClosestPointOnTetrahedron", "namespace_closest_point.html#a8d7febd22a8da31914181b84a7d8ac81", null ],
-      [ "GetClosestPointOnTriangle", "namespace_closest_point.html#a0d42c8adede82636667338badcc482b5", null ],
+      [ "GetClosestPointOnTetrahedron", "namespace_closest_point.html#abafebdbe3dea0fe6674c20fc9ccc8540", null ],
+      [ "GetClosestPointOnTriangle", "namespace_closest_point.html#a25fcaaf786d8724b11d02406ab74e0bc", null ],
       [ "OriginOutsideOfPlane", "namespace_closest_point.html#ad4bc500e8c3ea1bc2b2bc1a289408f1b", null ],
       [ "OriginOutsideOfTetrahedronPlanes", "namespace_closest_point.html#a7221bb45d1aec01527f66683bf2611e0", null ]
     ] ],
